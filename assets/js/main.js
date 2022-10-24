8a7089013e78a61fceee26a92498ef43be9a278d
mecarm <melecarmine99@gmail.com>
2022-10-24 10:45:57 +0200

    codice più ordinato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
-
-
 for(let x = 1; x <= 100; x++){
+
     let div = document.createElement('div')
     div.innerText = ""
-    if(x % 3 == 0 && x % 5 == 0){
-        div.innerText += "FizzBazz";
+    if(x % 3 == 0 && x % 5 == 0) {
+        div.innerText += "FizzBuzz";
         div.classList.add("red");
-    }else if(x % 5 == 0) {
+    } else if(x % 5 == 0) {
         div.classList.add("yellow");
         div.innerText += "Buzz";
-    }else if(x % 3 == 0){
+    } else if(x % 3 == 0) {
         div.classList.add("green");
         div.innerText += "Fizz";
-    }else{
+    } else {
         div.innerText += x;
         div.classList.add("blu");
     }
-    document.getElementById('box-cont').appendChild(div);
+    document.getElementById('box-cont').append(div);
 }
